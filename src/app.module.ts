f81669101ab1de1020bb0d4a3377edce48871917
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-03 15:39:20 -0500

    utilizando useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
@@ -8,7 +8,7 @@ const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, HttpModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -19,6 +19,21 @@ const API_KEY_PROD = 'PROD1212121SA';
 
       // Otro ejemplo: El API_KEY token se resolverá dependiendo de la variable de ambiente process.env.NODE_ENV
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS', //Nombre del Proveedor
+      // 👈 implement useFactory de manera async
+      useFactory: async (http: HttpService) => {
+        // Ejemplo realizando una peticion a una API REST
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        // Retornamos la informacion
+        return tasks.data;
+      },
+      // useFactory permite realizar una injeccion de dependencias
+      // En teste caso se injecta el HttpService
+      inject: [HttpService],
     },
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,14 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {} // 👈 Injectando API_KEY de manera segura
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[], // 👈 inject TASKS el cual es un array de cualquier cosa
+  ) {} // 👈 Injectando API_KEY de manera segura
 
   getHello(): string {
+    console.log(this.tasks); // 👈 print TASKS
+
     // Imprimos el valor de inyectado
     return `Hello World! ${this.apiKey}`;
   }
