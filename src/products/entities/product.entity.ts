24e7790a91226601c762fd5e435d05f7c320a09b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-04 13:25:43 -0500

    Implementando Mongoose en Módulos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,24 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema() // El decorador @Schema() marca una clase como definición de esquema.
+export class Product extends Document {
+  // Product extiende de document
+
+  @Prop({ required: true }) // Indicamos que la variable sera una propiedad y sera requerida
   name: string;
+
+  @Prop() // Indicamos que la variable sera una propiedad
   description: string;
+
+  @Prop({ type: Number }) // Indicamos que la variable sera una propiedad y sera numerica
   price: number;
+
+  @Prop({ type: Number }) // Indicamos que la variable sera una propiedad y sera numerica
   stock: number;
+
+  @Prop() // Indicamos que la variable sera una propiedad
   image: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product); // Exportamos un esquema apartir de la clase Product
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './entities/product.entity';
 
 import { ProductsController } from './controllers/products.controller';
 import { BrandsController } from './controllers/brands.controller';
@@ -8,6 +10,16 @@ import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 
 @Module({
+  imports: [
+    // Products.module se encargar de manejar a esta entidad
+    //Configuramos el modulo
+    MongooseModule.forFeature([
+      {
+        name: Product.name, // Indicamos el nombre del esquema
+        schema: ProductSchema, // El esquema a utilizar
+      },
+    ]),
+  ],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   // Nota: Como cada modulo es aislado podemos exporta un para que cualquier otro modulo lo pueda usar
