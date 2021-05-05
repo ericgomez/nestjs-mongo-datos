a764d62453a975e8e18f1d4bad31f7b69e198dcf
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-05-05 13:00:58 -0500

    Implementando Relaciones uno a muchos (embebidas)

--- src/users/dtos/customer.dto.ts
+++ src/users/dtos/customer.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsArray, // 👈 new decorator
+  ValidateNested, // 👈 new decorator
+} from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
 
 export class CreateCustomerDto {
@@ -14,6 +20,10 @@ export class CreateCustomerDto {
   @IsPhoneNumber() // Decorador para validar que sea un numero
   @IsNotEmpty() // Decorador para validar campos no vacios
   readonly phone: string;
+
+  @IsArray() // Decorador para validar que sea un Array
+  @IsNotEmpty() // Decorador para validar campos no vacios
+  readonly skills: any; // 👈 El atributo puede recibir cualquier tipo de dato
 }
 
 // PartialType permite reutilizar codigo utilizarndo las mismas validaciones y caracteristicas del clase que se extiende (CreateCustomerDto)
--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema() // El decorador @Schema() marca una clase como definición de esquema.
 export class Customer extends Document {
@@ -13,6 +13,11 @@ export class Customer extends Document {
 
   @Prop() // Indicamos que la variable sera una propiedad
   phone: string;
+
+  @Prop({
+    type: [{ name: { type: String }, color: { type: String } }], // Indicamos que la variable sera una propiedad de tipo array de tipo string con dos campos nombre y color
+  })
+  skills: Types.Array<Record<string, any>>; // 👈 La variable sera de tipo Array y como es embebido utilizamos Record
 }
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer); // Exportamos un esquema apartir de la clase Customer
